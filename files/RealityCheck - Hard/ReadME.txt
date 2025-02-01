bbdaa80795fc03e484d1b25659391055f3c9ff23
Raven-s-Soul <Sariotedesco@gmail.com>
2025-02-01 03:58:38 +0000

    update

---
+++ files/RealityCheck - Hard/ReadME.txt
@@ -0,0 +1,11 @@
+Programma prende in input 2 nomi di file e copia il file sorgente nel file destinazione.
+Gestisci casi di errori.
+
+example usa 'a' come file d'input e 'b' come file vuoto di output.
+
+console:
+
+> make
+> "./a.out" or "make run"
+> a
+> b
---
+++ files/RealityCheck - Hard/RealityCheck.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//      #include <stdio.h>
+// 	Debug Macro
+#define DEBUG_LOG 0
+//	1 - Active , 0 - Deactive
+#if DEBUG_LOG
+#define LOG(fmt, ...) printf(fmt "\n", ##__VA_ARGS__);
+#else
+#define LOG(fmt, ...) ;
+#endif
+
+int dimensione(int fd){
+        char c;
+        int i = 1;
+        int dim=0;
+        while(i > 0){
+                i=read(fd,&c,sizeof(char));
+                LOG("Byte letti %d",i)
+		dim=dim+1;
+        }
+        return dim;
+}
+
+void copy(char* s, char* d){
+
+        int fds=open(s, O_APPEND | O_RDONLY,S_IRWXU);
+        lseek(fds,0,SEEK_SET);
+
+        int fdd=open(d, O_CREAT | O_TRUNC | O_RDWR,S_IRWXU | S_IRWXO | S_IRGRP | S_IWOTH);
+
+        printf("\nMetto da %s -> %s\n",s,d);
+
+        if(fds==-1){
+                printf("\nErrore nell'apertura del file sorgente\n");
+                exit(-1);
+        }
+        if(fdd==-1){
+                printf("\nErrore nell'apertura del file destinatario\n");
+                exit(-1);
+        }
+
+        //calcolo dimensione del file sorgente
+        int dim=dimensione(fds);
+        //char *appoggio=(char*)malloc(dim);
+        char appoggio[dim];
+        lseek(fds,0,SEEK_SET);
+        read(fds,appoggio,dim*sizeof(char));
+	// Clean Garbage
+	appoggio[dim-1] = '\0';
+        printf("%s: %s", s , appoggio);
+	int t = write(fdd,appoggio,dim*sizeof(char));
+        //LOG("Totali: %d usati %d",dim, t)
+        lseek(fdd,0,SEEK_SET);
+        read(fdd,appoggio,dim);
+	printf("%s: %s", d , appoggio);
+        //free(appoggio);
+        close(fds);
+        close(fdd);
+}
+
+int main(){
+        printf("\nInserisci nome del file sorgente: ");
+        char s[50];
+        scanf("%s%*c",s);
+        printf("\nInserisci nome del file destinatario: ");
+        char d[50];
+        scanf("%s%*c",d);
+
+        LOG("%s",s)
+        LOG("%s",d)
+
+        copy(s,d);
+        printf("Copia terminata\n");
+        return 0;
+}
